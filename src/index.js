2b18a327c3c23d04a34aadf30425be94410d5301
Sean Wilkinson <sean@seanwilkinson.me>
2017-09-19 15:31:51 +0200

    Adding changes from Section 1 of course

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = "AIzaSyCQ7L8IcNsJLOd6HevsDX4n8VuwcCDuUuA"
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Create a new component, this component will produce HTML
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+);
+}
+
+// Take this HTML and put it on the the page in DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
